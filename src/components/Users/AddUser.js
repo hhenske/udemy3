7c3a7d9cd6685fa69aa7a2ac21962dcda331140e
Holly Henske <hcover333@gmail.com>
2021-08-30 07:47:58 -0500

    first commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser/module.css';
+
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefautl();
+    };
+
+    return (
+        <Card className={classes.input}>
+            <form onSUbmit = {addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age(years)</label>
+                <input id="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+    );
+};
+
+
+
+export default AddUser;
